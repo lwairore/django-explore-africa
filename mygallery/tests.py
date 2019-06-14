a0a817de10d9c515596b182a6e3bbc471a791212
lwairore <kwairore@gmail.com>
2019-06-14 20:56:44 +0300

    Create LocationTestCase which is the test class for Location class. Create a setUp method and then instantiate an instance of Location, self.nakuru

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -2,4 +2,7 @@ from django.test import TestCase
 from .models import Location, Category, Image
 
 # Create your tests here.
-
+class LocationTestCase(TestCase):
+    def setUp(self):
+        self.nakuru = Location(name='Nakuru')
+        
