030d7fe9332bccbe53a710d01320243c2c9e5a36
lwairore <kwairore@gmail.com>
2019-06-14 21:05:38 +0300

    Create save_location() method that calls save() method on an instance of Location

--- mygallery/models.py
+++ mygallery/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='SOME STRING')
+    def save_location(self):
+        self.save()
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, default='SOME STRING')
 
 class Image(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, default='SOME STRING')
     description = models.TextField()
-    location = models.ForeignKey(Location)
+    location = models.ForeignKey(Location,default=1)
+    category = models.ForeignKey(Category, default=1)
