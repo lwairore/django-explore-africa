6f9b69c669b0754695ffc2695abf5e4e34dc7632
lwairore <kwairore@gmail.com>
2019-06-15 13:08:43 +0300

    Import Image class from models. Query the database for all images in Image table and store them in images variable. Sent images to index.html

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 # Create your views here.
 
 def home(request):
-    return render(request,'homepage/index.html')
+    images = Image.objects.all()
+    return render(request,'homepage/index.html', {'images':images})
+    
