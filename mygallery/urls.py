60af9377ed0a12c374e9ca08e9490eddfc71ada2
lwairore <kwairore@gmail.com>
2019-06-15 13:17:34 +0300

    Create a new URLpattern that references the search_results function in views. Then name that URL to search_results.

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    url('^$', views.home, name='home')
+    url('^$', views.home, name='home'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIAL_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
