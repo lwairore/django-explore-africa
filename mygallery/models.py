b0f17f6081c9c470a53af3fc2ae98dc6bec07ba4
lwairore <kwairore@gmail.com>
2019-06-15 10:12:44 +0300

    Create save_image method that calls save method on a Image instance. Create delete_image method that calss delete method on a Image instance. Create get_image_by_id() class method that takes in id as a parameter. The class method queries the database and returns only the image with the same id as it has passed it.

--- mygallery/models.py
+++ mygallery/models.py
@@ -5,16 +5,35 @@ class Location(models.Model):
     name = models.CharField(max_length=100, default='SOME STRING')
     def save_location(self):
         self.save()
-        
+
     def delete_location(self):
         self.delete()
 
 
 class Category(models.Model):
     name = models.CharField(max_length=30, default='SOME STRING')
+    def save_category(self):
+        self.save()
+    
+    def delete_category(self):
+        self.delete()
 
 class Image(models.Model):
     name = models.CharField(max_length=30, default='SOME STRING')
     description = models.TextField()
     location = models.ForeignKey(Location,default=1)
     category = models.ForeignKey(Category, default=1)
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls):
+        cls.objects.filter(name='Tourism').update(name='image')
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id)
+        return image
