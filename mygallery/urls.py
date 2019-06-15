fd5a4c2968b8310b172ee5527e3ed6b846df92aa
lwairore <kwairore@gmail.com>
2019-06-15 12:39:47 +0300

    Import django.conf module and a static function. Add urlpatterns a new static route that referneces the location to the uploaded file.

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     url('^$', views.home, name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIAL_URL, document_root=settings.MEDIA_ROOT)
