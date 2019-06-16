6a15c03b545f0f15895f70ea86bef33ef7a6c1e1
lwairore <kwairore@gmail.com>
2019-06-16 15:09:42 +0300

    import HttpResponse, Http404 from django.http. Import Image, Category and Location classes from .models . Define search_results() function that takes one parameter, request. Inseide search_results function: Use an if statement to assertain if the user submits the submit form and if the submit form is with a value. Use try function: create a variable search_term that stores the value entered by the user. Retrieve all categories from the database and store them in category_all. Declare a variable, images_final that is an empty list. Use a for loop to loop over category_all, and store each item in variable i. USe an if statement to compare the search_term entered by the user and all the values in category_all. If a match is found, we extract it's id. Call the search_image method an pass it the matched categories name id, and store the returned value in searched_images. Append searched_images to images_final.

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Image
-# Create your views here.
+from django.http import HttpResponse, Http404
+from .models import Image, Category, Location
+from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
     images = Image.objects.all()
-    return render(request,'homepage/index.html', {'images':images})
+    locations = []
+    locations_all = Location.objects.all()
     
+    for i in locations_all:
+        if i.name not in locations:
+            locations.append(i.name)
+    print(locations)
+            
+    return render(request,'homepage/index.html', {'images':images, 'locations':locations})
+    
+def search_results(request):
+    if 'category' in request.GET and request.GET['category']:
+            try: 
+                search_term = request.GET.get('category') 
+                print(Category.objects.all())
+                category = Category.objects.filter(name=search_term)
+                print('Primary key',category)
+                print(search_term)
+            
+                category_all = Category.objects.all()
+            
+
+                images_final = []
+                for i in category_all:
+                    if search_term == i.name:
+                        print(i.id)
+                        searched_images = Image.search_image(i.id)
+                        images_final.append(searched_images)
+                        print(searched_images)
+                        print(images_final)
+                message = f'{search_term}'
+                return render(request, 'search/search_results.html', {'message':message, 'searched_images':searched_images, 'images_final':images_final })
+
+            except:
+                raise Http404()
+                    
+    else: 
+        message = 'You haven\'t searchded for any term'
+        return render(request, 'search/search_results.html', {'message':message})
+
