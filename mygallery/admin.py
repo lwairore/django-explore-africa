b344fd59e052631fb42d1dcf61e1d46436679931
lwairore <kwairore@gmail.com>
2019-09-04 18:48:23 +0300

    Import AboutMe class. Call register function from 'admin.site' to add AboutMe to Admin dashboard.

--- mygallery/admin.py
+++ mygallery/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Location, Category, Image
+from .models import Location, Category, Image, AboutMe
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Category)
 admin.site.register(Image)
+admin.site.register(AboutMe)
